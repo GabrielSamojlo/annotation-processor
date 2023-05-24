3c228dfe39685c92be4e8b3d7eb515db22c0c330
GabrielSamojlo <gabrielsamojlo@gmail.com>
2023-05-24 16:56:18 +0200

    migrate to KSP

--- app/src/main/java/com/example/annotationprocessing/MainActivity.kt
+++ app/src/main/java/com/example/annotationprocessing/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.annotationprocessing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.lang.String
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +10,7 @@ class MainActivity : AppCompatActivity() {
 
         val userDto = UserDto(
             id = 10,
-            email = String("gabriel.samojlo@netguru.com"),
+            email = "gabriel.samojlo@netguru.com",
             accountBalance = 10.0
         )
     }
---
+++ myproc/src/main/java/com/example/myproc/MyProcessorProvider.kt
@@ -0,0 +1,14 @@
+package com.example.myproc
+
+import com.google.devtools.ksp.processing.SymbolProcessor
+import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
+import com.google.devtools.ksp.processing.SymbolProcessorProvider
+
+class MyProcessorProvider : SymbolProcessorProvider {
+    override fun create(
+        environment: SymbolProcessorEnvironment
+    ): SymbolProcessor {
+        return MyProcessor(environment.codeGenerator)
+    }
+}
+
